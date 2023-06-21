88d7763a4315681f2497a81db7e870ae5f2a2cff
0nyr <rascoussier.florian@gmail.com>
2023-06-21 19:39:04 +0200

    Correction session_8.

--- exo_8/uml_aufgabe_3.py
+++ exo_8/uml_aufgabe_3.py
@@ -12,6 +12,7 @@ dot.node('A', '''<<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">
             <TR><TD>Type</TD></TR>
             <TR><TD>Age</TD></TR>
             <TR><TD>Image</TD></TR>
+            <TR><TD>FK: features</TD></TR>
         </TABLE>
     </TD></TR></TABLE>>''')
 
@@ -21,6 +22,7 @@ dot.node('K', '''<<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">
         <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
             <TR><TD>Name</TD></TR>
             <TR><TD>Pass-photo</TD></TR>
+            <TR><TD>FK: features</TD></TR>
         </TABLE>
     </TD></TR></TABLE>>''')
 
@@ -33,7 +35,7 @@ dot.node('C', '''<<TABLE BORDER="0" CELLBORDER="0" CELLSPACING="0">
     </TD></TR></TABLE>>''')
 
 # Relationships with cardinalities
-dot.edge('A', 'C', headlabel='1', taillabel='1', labeldistance='2.5', labelangle='45', fontsize='20')
+dot.edge('A', 'C', headlabel='1', taillabel='*', labeldistance='2.5', labelangle='45', fontsize='20')
 dot.edge('C', 'K', headlabel='1..*', taillabel='*', labeldistance='2.5', labelangle='-45', fontsize='20')
 
 # Save the output
